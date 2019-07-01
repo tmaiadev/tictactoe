f2d9f225e3739e40f83c9a443e39edc87a26c149
Thalles Maia <tmaiadev@gmail.com>
2019-07-01 09:04:50 -0300

    Display score

--- src/components/score/score.js
+++ src/components/score/score.js
@@ -1,10 +1,78 @@
-import React from 'react';
+import React, {
+  useState,
+  useEffect,
+  useContext
+} from 'react';
+import Icon from '../icon/icon';
+import GameStateContext from '../../contexts/gameState';
 import './score.css';
 
 function Score() {
+  const [gameState] = useContext(GameStateContext);
+  const [oScore, setOScore] = useState(gameState.score.you);
+  const [xScore, setXScore] = useState(gameState.score.opponent);
+
+  useEffect(() => {
+    const { you, opponent } = gameState.score;
+    if (you === oScore && opponent === xScore) return;
+
+    // Get score number and hide it
+    const $n = document.getElementById(`${you !== oScore ? 'o' : 'x'}-score`);
+    $n.classList.add('score__number--hide');
+    $n.addEventListener('transitionend', () => {
+      // After the animation, we set the number
+      // to will-update position
+      $n.classList.remove('score__number--hide');
+      $n.classList.add('score__number--will-update');
+      
+      // We update the score
+      if (oScore !== you) {
+        setOScore(you);
+      } else {
+        setXScore(opponent);
+      }
+
+      
+      $n.addEventListener('transitionend', () => {
+        $n.classList.remove('score__number--will-update');
+        $n.classList.add('ready');
+      }, { once: true });
+    }, { once: true });
+  }, [gameState, oScore, setOScore, xScore, setXScore]);
+
   return (
     <div className="score">
-      0 x 0
+      <div />
+      <div className="score__cell">
+        <div className="score__img">
+          <Icon
+            type="o"
+            color="var(--primary-color)"
+            size={40}
+          />
+        </div>
+        <div className="score__number-holder">
+          <div className="score__number" id="o-score">
+            {oScore}
+          </div>
+        </div>
+      </div>
+      <div />
+      <div className="score__cell">
+        <div className="score__img">
+          <Icon
+            type="x"
+            color="var(--secondary-color)"
+            size={40}
+          />
+        </div>
+        <div className="score__number-holder">
+          <div className="score__number" id="x-score">
+            {xScore}
+          </div>
+        </div>
+      </div>
+      <div />
     </div>
   )
 }
